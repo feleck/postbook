9acdf66523888a90ed0aa34b12dc964fac998d87
feleck <feleck@poczta.onet.pl>
2010-09-24 14:06:03 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postbook::Application.config.secret_token = '9288cc577418f73a88a690ad9b0fac72cb253eaef5f37857284fb01f8544015bf253f1b7cfbe468f6bc4de8d4f0fe699bee0225434d24c55e3978c6ebb1dbb74'
