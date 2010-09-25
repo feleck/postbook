2a767a032ffcea1791c11bac251e98b4d0c0f597
feleck <feleck@poczta.onet.pl>
2010-09-25 11:50:21 +0200

    With exercisec - help page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Postbook::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
   get "pages/about"
+  get 'pages/help'
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,9 @@ describe PagesController do
 # added:
   render_views
 
+  before(:each) do
+    @base_title = "PostBook App"
+  end
   describe "GET 'home'" do
     it "is successful" do
       get 'home'
@@ -12,7 +15,7 @@ describe PagesController do
 
     it "has the right title" do
       get 'home'
-      response.should have_selector("title", :content => "PostBook App | Home")
+      response.should have_selector("title", :content => @base_title +" | Home")
     end
   end
 
@@ -23,7 +26,7 @@ describe PagesController do
     end
     it "has the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "PostBook App | Contact")
+      response.should have_selector("title", :content => @base_title +" | Contact")
     end
   end
 
@@ -34,8 +37,19 @@ describe PagesController do
     end
     it "has the right title" do
       get 'about'
-      response.should have_selector("title", :content => "PostBook App | About")
+      response.should have_selector("title", :content => @base_title +" | About")
     end
+  end
+  describe "GET 'help'" do
+    it "is successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "has the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title +" | Help")
+    end
+    
   end
   
 end
